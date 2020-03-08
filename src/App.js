8d136c4cc6c43fed1247996125f230b036aff338
sarhan-azizov <sarhan.azizov@gmail.com>
2020-03-08 19:02:59 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button } from 'microfrontend-common-ui';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <Button>microfrontend-common-ui</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
